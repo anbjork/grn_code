8c51a251d0104830885c2ccd9fb073b11ae39aee
anbjork <anton.bjork@scilifelab.se>
2026-04-07 17:15:24 +0200

    documentation update

--- preprocess_reference_network.py
+++ preprocess_reference_network.py
@@ -21,17 +21,24 @@ net_mat.sort_index(inplace=True, axis=1)
 
 
 
+# Old, just wanted to verify that the old network I used was the same as a
+# fresh Beeline network. It was, which was nice. It matters less now, since I am using
+# a fresh Beeline network and have better track of the code and origins this time.
+# The path is not found, and I think it is on Eris, not the one I am working on now.
+# Leaving this code here for posterity, mostly because my brain is too tired to make
+# the decision to remove it right now.
+# Deleting shit becomes more worrying when tired somehow haha
 
 
-other_net = anton_util.unpickle_object('/home/anbjork/projects/small/replogle_grns/versions/33/replogle_round_2/data/Non-specific-ChIP-seq-network_with_weights.pkl')
-
-other_net.sort_index(inplace=True)
-other_net.sort_index(inplace=True, axis=1)
-
-
-print('All elements in new net equals old net after sorting: ')
-print((net_mat == other_net).all().all())
-
-
+# other_net = anton_util.unpickle_object('/home/anbjork/projects/small/replogle_grns/versions/33/replogle_round_2/data/Non-specific-ChIP-seq-network_with_weights.pkl')
+#
+# other_net.sort_index(inplace=True)
+# other_net.sort_index(inplace=True, axis=1)
+#
+#
+# print('All elements in new net equals old net after sorting: ')
+# print((net_mat == other_net).all().all())
+#
+#
 
 
