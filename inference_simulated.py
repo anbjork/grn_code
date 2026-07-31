602cbeb7fddda5624f8a0541b1b4f1a85cdb6cc8
anbjork <anton.bjork@scilifelab.se>
2026-07-31 15:48:54 +0200

    overwrite inferences previously in pipline
    and some updates of default inference methods to run

--- inference_simulated.py
+++ inference_simulated.py
@@ -3,25 +3,26 @@ from pathlib import Path
 from pipeline_functions import inference
 import functions
 
+base_path = Path('outputs__in_pipeline/simulated')
+(base_path / 'inferences.pkl').unlink(missing_ok=True)
+
 functions = [
-    # functions.fast_methods_inference,
-    # functions.random_inference,
-    # functions.correlation_inference,
-    functions.inspre_inference,
+    functions.fast_methods_inference,
+    functions.random_inference,
+    functions.correlation_inference,
+    # functions.inspre_inference,
     # functions.inspre_inference_hdf5,
     # functions.psgrn_inference,
     # functions.genie3_inference,
     # functions.deepsem_inference,
     # functions.dspin_inference,
+    # functions.dspin_inference_wrapper,
     ]
 
 for method_function in functions:
-    base_path = Path('outputs__in_pipeline/simulated')
     inference(
         data_path = base_path / 'data_processed.pkl',
         output_path = base_path / 'inferences.pkl',
         method_function = method_function
         )
-
-
 
