e00f715a4fec4b422d2d0211147df87c1fdeab05
anbjork <anton.bjork@scilifelab.se>
2026-08-06 16:22:46 +0200

    simulation runs out of the box! Both genespider and genesnake
    regarding genesnake; It reports parameters as if it was for genespider. Which is not accurate. It uses the ones it can, changes some others, and ignores some. I probably used this to investigate transpositions for lsco. To compare behaviors between genesnake and genespider data
    
    removed genesnake commit pin and installation from setup script. I probably want to use the same genesnake throughout the repo. Setup will get a bit messy otherwise. I remember I added the pin just because I was pinning genespider anyways, and was having the general realisation that stuff is not reproducible if not using versions, releases, or commit pins. So I don't expect any functionality specific that that commit.
    
    On the other hand, I very deliberately kept the genespider commit pin, since that is the commit before the unannounced and most likely untested change in noise model.

--- data_simulation/setup.sh
+++ data_simulation/setup.sh
@@ -9,10 +9,3 @@ cd genespider
 git checkout 37ad0c97adbfe52892040de74f26d90acd9b7cce
 cd ../
 
-git clone git@bitbucket.org:sonnhammergrni/genesnake.git
-cd genesnake/
-# Pinning for reproducibility, since no proper versioning
-git checkout 72a16a0e5b91e528880b742553f0161db79c81f6
-pip install -e .
-cd ../
-
--- data_simulation/simulate_for_replogle_with_genesnake.sh
+++ data_simulation/simulate_for_replogle_with_genesnake.sh
@@ -1,6 +1,6 @@
 
 python configure_simulations.py
-python simulate.py
+python simulate_genesnake.py
 python gather_simulation_data.py
 
 
