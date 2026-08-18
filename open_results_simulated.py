fae386ee12490f00ab960fb4e8e59afcc579236e
anbjork <anton.bjork@scilifelab.se>
2026-08-18 12:51:59 +0200

    chore: adjust filtering

--- open_results_simulated.py
+++ open_results_simulated.py
@@ -10,7 +10,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-df = df.drop('f1_scores', axis = 1)
+# df = df.drop('f1_scores', axis = 1)
 df = df.rename({'n_genes_after_harmonisation': 'n_genes'}, axis = 1)
 
 # cs = ['is_shuffled', 'method', 'transform', 'delta']
@@ -19,5 +19,5 @@ if all([elem in df.columns for elem in cs]):
     df = df.sort_values(by = cs)
 dfd = df.reset_index(drop = True)
 
-print(dfd)
+# print(dfd)
 
