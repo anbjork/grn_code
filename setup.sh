01e3800d46650ed22d44ea428fb9da185159dd80
anbjork <anton.bjork@scilifelab.se>
2026-09-01 18:43:03 +0200

    bugfix: corrected repo url

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd src/grn_code/
 cp pipeline_configuration_template.py pipeline_configuration.py
 cd -
 
-git clone https://antonbjork@bitbucket.org/sonnhammergrni/genesnake.git
+git clone git@bitbucket.org:sonnhammergrni/genesnake.git
 
 cd genesnake
 pip install -e .
