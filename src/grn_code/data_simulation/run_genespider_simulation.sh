aa3f95f86ab0ac8763e5803fed5670ca2069345a
anbjork <anton.bjork@scilifelab.se>
2026-09-11 17:45:36 +0200

    separate config for data cases; move files

--- src/grn_code/data_simulation/run_genespider_simulation.sh
+++ src/grn_code/data_simulation/run_genespider_simulation.sh
@@ -1,5 +1,5 @@
 
-# python configure_simulations.py
+# python configure_simulations_factorial.py
 python configure_simulations_data_cases.py
 python simulate_genespider.py
 python gather_simulation_data.py
