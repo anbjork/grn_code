194ec29d4750c838eba6663cf233d58e0f3969b3
anbjork <anton.bjork@scilifelab.se>
2026-09-16 15:22:19 +0200

    wip; merge setup scripts

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ cd genesnake
 pip install -e .
 cd -
 
-cd src/grn_code/data_simulation
-bash setup.sh
-cd -
+git clone git@bitbucket.org:sonnhammergrni/genespider.git
+cd genespider
+# Changes to suitable commit;
+# The one for the publication, before sneak noise change
+# Since then, have also branched, and fixed noise for control cells
+git checkout dcb208e17da01af65cfe0b92e59a822c03288361
+cd ../
 
 pip install numpy pandas matplotlib anton_util h5py
 
--- src/grn_code/data_simulation/setup.sh
+++
@@ -1,9 +0,0 @@
-
-git clone git@bitbucket.org:sonnhammergrni/genespider.git
-cd genespider
-# Changes to suitable commit;
-# The one for the publication, before sneak noise change
-# Since then, have also branched, and fixed noise for control cells
-git checkout dcb208e17da01af65cfe0b92e59a822c03288361
-cd ../
-
