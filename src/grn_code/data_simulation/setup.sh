c68ec719be27b02462c9b56b43df5075628f6520
anbjork <anton.bjork@scilifelab.se>
2026-09-16 15:18:16 +0200

    wip; restructure; change genespider commit, to branch with dropouts for control cells

--- src/grn_code/data_simulation/setup.sh
+++ src/grn_code/data_simulation/setup.sh
@@ -1,10 +1,9 @@
 
-rm -rf genespider
-
 git clone git@bitbucket.org:sonnhammergrni/genespider.git
 cd genespider
 # Changes to suitable commit;
 # The one for the publication, before sneak noise change
-git checkout 37ad0c97adbfe52892040de74f26d90acd9b7cce
+# Since then, have also branched, and fixed noise for control cells
+git checkout dcb208e17da01af65cfe0b92e59a822c03288361
 cd ../
 
