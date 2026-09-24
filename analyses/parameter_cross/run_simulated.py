e34e6ed14807f4d4735d70b89f6166d846c21653
anbjork <anton.bjork@scilifelab.se>
2026-09-24 13:22:56 +0200

    restored parameter cross driver script
    following reverting earlier config handling change
    
    it was copied from the data cases analysis after I made the earlier change, and so revert didnt affect this one

--- analyses/parameter_cross/run_simulated.py
+++ analyses/parameter_cross/run_simulated.py
@@ -4,22 +4,33 @@ import anton_util
 from grn_code.paths_anchor import output_base_path
 output_path = output_base_path / 'parameter_cross'
 
+make_config = False
+# make_config = True
+
+# read_simulation_specifications = False
+read_simulation_specifications = True
+
 config_name = 'configuration.pkl'
 p = output_path / config_name
-
-if p.exists():
-    config = anton_util.unpickle_object(p)
-else:
+if make_config:
     from configuration import configure
     config = configure()
     output_path.mkdir(exist_ok=True, parents=True)
     anton_util.pickle_object(config, p)
+else:
+    try:
+        config = anton_util.unpickle_object(p)
+    except FileNotFoundError:
+        print(f'{p} not found')
+        import sys
+        sys.exit(1)
 
 from grn_code.pipeline_code import run_pipeline
 run_pipeline(
         config = config,
         output_base_path = output_path,
-        )
+        read_simulation_specifications = read_simulation_specifications)
+
 
 from plot_results import plot_metrics
 plot_metrics(output_path, major_col='metric', minor_col='data_case')
