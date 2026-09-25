8db914f9612351df1ff233a4d00efd9f5a972a5d
anbjork <anton.bjork@scilifelab.se>
2026-09-25 13:20:54 +0200

    updated manual results handling

--- analyses/parameter_cross/manually_handle_results.py
+++ analyses/parameter_cross/manually_handle_results.py
@@ -2,14 +2,16 @@
 import anton_util
 
 output_types = [
-        'data_processed',
+        'preprocessed_data',
         'reference_networks',
         'inferences',
         'benchmarks',
+        'compiled_results',
         ]
 
+from local_imports import output_path
 # base_path = 'outputs/in_pipeline'
-base_path = 'outputs/simulated'
+base_path = f'{output_path}/simulated'
 
 outputs = []
 for output_type in output_types:
@@ -27,23 +29,23 @@ anton_util.log_timestamp('done reading')
 
 # Note; Only for convenience, and how contents of outputs are written to disk
 # in the save function prepared below
-data_processed, reference_networks, inferences, benchmarks = outputs
-# Convenience alias
-datasets = data_processed
+preprocessed_data, reference_networks, inferences, benchmarks, compiled_results = outputs
 
 
 
+def extract_meta_data(datasets):
+    from copy import deepcopy
+    metas = deepcopy([d['meta'] for d in datasets])
+    for meta in metas:
+        for k, v in meta['dataset_parameters'].items():
+            meta[k] = v
+        meta.pop('dataset_parameters')
+    dataset_dict = {}
+    for meta, data in zip(metas, datasets):
+        dataset_dict[frozenset(meta.items())] = data
+    return metas, dataset_dict
 
 
-from copy import deepcopy
-metas = deepcopy([d['meta'] for d in datasets])
-for meta in metas:
-    for k, v in meta['dataset_parameters'].items():
-        meta[k] = v
-    meta.pop('dataset_parameters')
-dataset_dict = {}
-for meta, data in zip(metas, datasets):
-    dataset_dict[frozenset(meta.items())] = data
 
 
 
@@ -55,7 +57,7 @@ for meta, data in zip(metas, datasets):
 
 # Checking dropouts in a convenient fashion
 dropouts = []
-for d in data_processed:
+for d in preprocessed_data:
     cols = {k: v for k, v in d['meta'].items() if 'fraction' in k}
     tmp = 'data_case'
     cols[tmp] = d['meta']['dataset_parameters'][tmp]
